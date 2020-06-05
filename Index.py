33fdfb3896fc026e9a0ccd5d80689fb8ad4eef7a
Marcos Villatoro <marcosdanii27@gmail.com>
2020-06-05 14:45:42 -0600

    borré la estructura que había creado, agregué 2 funciones y los 5 labels entry

--- Index.py
+++ Index.py
@@ -17,48 +17,81 @@ Titulo = Label(MyFrame, text="Bienvenido")
 Titulo.grid(row=1, column=1)
 Titulo.config(font=("comic sans ms",20))
 
-abc = StringVar()
-ingreso = StringVar()
-xyz = IntVar()
-pqr = IntVar()
-out = IntVar()
+nombre = StringVar()
+apellido = StringVar()
+day = IntVar()
+month = IntVar()
+year = IntVar()
+
+def ContandoLosDías():
+    StrFecha = f"{year.get()}-{month.get()}-{day.get()}"
+    date_object = datetime.strptime(StrFecha, '%Y-%m-%d')
+    today = datetime.today()
+
+    x1 = today
+    x2 = date_object
+    respuesta1 = abs(x1-x2).days
+
+    respuesta1 = f"tu fecha de nacimiento es{date_object} y ha vivido {respuesta1}"
+
+    lblresp.configure(Text = respuesta1)
+
+    def ContandoLasLetras():
+        Nombre = f"{nombre.get()}"
+        Apellido = f"{apellido.get()}"
+
+        contar1 = len(nombre)
+        contar2 = len(apellido)
+
+        if contar1 % 2 == 0:
+            a1 = f"{nombre}el nombre ingresado es de numero par"
+        else:
+            a1 = f"{nombre}el nombre ingresado es de numero impar"
 
 
+        if contar2 % 2 == 0:
+             a2 = f"{apellido}el apellido ingresado es de numero par"
+        else:
+             a2 = f"{apellido} el apellido ingresado es de numero impar"
+
+        respuesta2 = f"{a1} y {a2}"
+
+        lblresp.configure(text = respuesta2)
+
+nombres = Label(MyFrame, text = "Nombre: ")
+nombres.grid(row=2, column=1)
+nombres.config(padx=5, pady=5)
+txtnombres = Entry(MyFrame, textvariable = nombre)
+txtnombres.grid(row=2, column=2)
+
+Apellidos = Label(MyFrame, text = "Apellido: ")
+Apellidos.grid(row=3, column=1)
+Apellidos.config(padx=10, pady=10)
+txtApellido = Entry(MyFrame, textvariable = apellido)
+txtApellido.grid(row=3, column=2)
+
+Días = Label(MyFrame, text = "Día: ")
+Días.grid(row=4, column=1)
+Días.config(padx=5, pady=5)
+txtDías = Entry(MyFrame, textvariable = nombre)
+txtDías.grid(row=4, column=2)
+
+Meses = Label(MyFrame, text = "Mes: ")
+Meses.grid(row=5, column=1)
+Meses.config(padx=5, pady=5)
+txtMeses = Entry(MyFrame, textvariable = nombre)
+txtMeses.grid(row=5, column=2)
+
+Años = Label(MyFrame, text = "Año: ")
+Años.grid(row=6, column=1)
+Años.config(padx=5, pady=5)
+txtAños = Entry(MyFrame, textvariable = nombre)
+txtAños.grid(row=6, column=2)
+
+btn1 = Button(MyFrame, text = "Función 1")
+btn1.grid(row = 7, column = 0)
+btn1.config(padx=8, pady=8)
 
 
-
-
-
-
-
-
-
-name = Label(text="Nombre", font=("Comic Sans ms", 14)).place(x=100, y=50)
-txt1 = Entry(master, textvar="abc").place(x=175, y=60)
-
-apellido = Label(text="Apellido",font = ("comic sans ms", 14)).place(x = 100, y = 80)
-txt2 = Entry(master, textvar = "ingreso").place(x = 175, y = 90)
-
-Día = Label(text="    Día  ",font = ("comic sans ms", 14)).place(x = 100, y = 110)
-txt3 = Entry(master, textvar = "xyz").place(x = 175, y = 120)
-
-Mes = Label(text="   Mes  ",font = ("comic sans ms", 14)).place(x = 100, y = 140)
-txt4 = Entry(master, textvar = "pqr").place(x = 175, y = 150)
-
-Año = Label(text="   Año  ",font = ("comic sans ms", 14)).place(x = 100, y = 170)
-txt5 = Entry(master, textvar = "out").place(x = 175, y = 180)
-
-
-
-
-btn1 = Button(master, text = "Función 1", font = ("Comic Sans ms", 10), width=8).place(x=20, y=210)
-
-btn2 = Button(master, text = "Función 2", font = ("Comic Sans ms", 10), width=8).place(x=95, y=210)
-
-btn3 = Button(master, text = "Función 3", font = ("Comic Sans ms", 10), width=8).place(x=170, y=210)
-
-btn4 = Button(master, text = "Función 4", font = ("Comic Sans ms", 10), width=8).place(x=245, y=210)
-
-btn5 = Button(master, text = "Función 5", font = ("Comic Sans ms", 10), width=8).place(x=320, y=210)
-
+    
 master.mainloop()
