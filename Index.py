c643cc842b96fc18a1d7decc5d5a6038710d3be2
Marcos Villatoro <marcosdanii27@gmail.com>
2020-06-04 23:49:11 -0600

    agregué los botones sin funcionalidad y cambié tamaños y alineación

--- Index.py
+++ Index.py
@@ -10,24 +10,34 @@ master.title("Examen Final")
 
 Titulo = Label(text="BIENVENIDOS", font=("comic sans ms", 20)).place(x=200, y=5)
 
-name = Label(text="Nombre", font=("Comic Sans ms", 14)).place(x=40, y=50)
+name = Label(text="Nombre", font=("Comic Sans ms", 14)).place(x=100, y=50)
 ingreso = StringVar()
-txt1 = Entry(master, textvar="ingreso").place(x=125, y=60)
+txt1 = Entry(master, textvar="ingreso").place(x=175, y=60)
 
-apellido = Label(text="Apellido",font = ("comic sans ms", 14)).place(x = 40, y = 80)
+apellido = Label(text="Apellido",font = ("comic sans ms", 14)).place(x = 100, y = 80)
 ingreso = StringVar()
-txt2 = Entry(master, textvar = "ingreso").place(x = 125, y = 90)
+txt2 = Entry(master, textvar = "ingreso").place(x = 175, y = 90)
 
-Día = Label(text="    Día  ",font = ("comic sans ms", 14)).place(x = 40, y = 110)
+Día = Label(text="    Día  ",font = ("comic sans ms", 14)).place(x = 100, y = 110)
 ingreso = StringVar()
-txt3 = Entry(master, textvar = "ingreso").place(x = 125, y = 120)
+txt3 = Entry(master, textvar = "ingreso").place(x = 175, y = 120)
 
-Mes = Label(text="   Mes  ",font = ("comic sans ms", 14)).place(x = 40, y = 140)
+Mes = Label(text="   Mes  ",font = ("comic sans ms", 14)).place(x = 100, y = 140)
 ingreso = StringVar()
-txt4 = Entry(master, textvar = "ingreso").place(x = 125, y = 150)
+txt4 = Entry(master, textvar = "ingreso").place(x = 175, y = 150)
 
-Año = Label(text="   Año  ",font = ("comic sans ms", 14)).place(x = 40, y = 170)
+Año = Label(text="   Año  ",font = ("comic sans ms", 14)).place(x = 100, y = 170)
 ingreso = StringVar()
-txt5 = Entry(master, textvar = "ingreso").place(x = 125, y = 180)
+txt5 = Entry(master, textvar = "ingreso").place(x = 175, y = 180)
+
+btn1 = Button(master, text = "Función 1", font = ("Comic Sans ms", 10), width=8).place(x=20, y=210)
+
+btn2 = Button(master, text = "Función 2", font = ("Comic Sans ms", 10), width=8).place(x=95, y=210)
+
+btn3 = Button(master, text = "Función 3", font = ("Comic Sans ms", 10), width=8).place(x=170, y=210)
+
+btn4 = Button(master, text = "Función 4", font = ("Comic Sans ms", 10), width=8).place(x=245, y=210)
+
+btn5 = Button(master, text = "Función 5", font = ("Comic Sans ms", 10), width=8).place(x=320, y=210)
 
 master.mainloop()
